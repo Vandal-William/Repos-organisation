3cf4e85fd9519eb2d7a285752c911adca50876a8
Vandal-William <vandal.william@hotmail.com>
2023-03-04 12:50:08 +0100

    create server

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+module.exports = {
+  apps : [{
+    name: 'portfolio',
+    script: 'index.js',
+    autorestart: true,
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : process.env.VPS_USER,
+      host : process.env.HOST,
+      ref  : process.env.GIT_REF,
+      repo : process.env.GIT_REPO,
+      path : process.env.VPS_PATH,
+      'pre-deploy-local': '',
+      'post-deploy' : 'pm2 startOrRestart ecosystem.config.js',
+      'pre-setup': ''
+    }
+  }
+};
+
---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const { Probot } = require('probot');
+
+const app = express();
+const port = 3100
+
+// Initialisation de l'app probot
+const probot = new Probot({
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    webhookProxy: `http://localhost:${port}`
+});
+
+// Middleware pour intercepter les requêtes GitHub
+app.use('/', (req, res, next) => {
+    // vérification de la signature HMAC (est ce que la requête vient de GitHub)
+    if (probot.webhooks.verify(req, res, next)){
+        // si la requête est valide, elle est passé a l'app probot
+        probot.webhooks.receive(req, res, next);
+    }else {
+        res.status(400).send('Invalid request');
+    }
+});
+
+// mise en place d'un ecouteur d'évènement qui écoute la création d'un répertoire GitHub
+
+probot.on('repository.created', async (context) => {
+    const repository = context.payload.repository;
+    const repoFullName = repository.full_name;
+
+    // code qui ajoute le webhook au dépôt Github 
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
