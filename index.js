6e6bc83e2e417010845bc597647e9527f9897852
Vandal-William <vandal.william@hotmail.com>
2023-03-04 14:09:01 +0100

    fix bug appId

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const app = express();
 const port = 3100
 
 // Initialisation de l'app probot
-const probot = new Probot({
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
-    webhookProxy: `http://localhost:${port}`
-});
+const probot = new Probot({appId: process.env.APP_ID, privateKey: process.env.PRIVATE_KEY});
 
 // Middleware pour intercepter les requêtes GitHub
 app.use('/', (req, res, next) => {
     // vérification de la signature HMAC (est ce que la requête vient de GitHub)
+    res.json("J'attends la création d'un nouveau repertoire github...")
     if (probot.webhooks.verify(req, res, next)){
         // si la requête est valide, elle est passé a l'app probot
         probot.webhooks.receive(req, res, next);
